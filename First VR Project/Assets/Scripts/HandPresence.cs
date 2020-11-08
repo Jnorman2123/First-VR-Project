972f9212e5828264583864aa3fb67dbf5d8844e3
Jonathan Norman <Jnorman2123@gmail.com>
2020-11-08 14:42:06 -0500

    Created right and left hand presence prefabs and added to right and left hand
    Created right and left hand presence prefabs and added to right and left hand

--- First VR Project/Assets/Scripts/HandPresence.cs
+++ First VR Project/Assets/Scripts/HandPresence.cs
@@ -10,7 +10,7 @@ public class HandPresence : MonoBehaviour
     // Declare new list of devices
     private List<InputDevice> devices;
     // Declare new set of input device characteristics
-    private InputDeviceCharacteristics rightControllerCharacteristics;
+    public InputDeviceCharacteristics controllerCharacteristics;
     // Declare targetDevice variable
     private InputDevice targetDevice;
     // Declare new game object variables
@@ -19,12 +19,11 @@ public class HandPresence : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Set devices to new list of input devices and rightControllerCharacteristics
+        // Set devices to new list of input devices
         devices = new List<InputDevice>();
-        rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
 
-        // Get devices with right controller characteristics
-        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+        // Get devices with controller characteristics
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
 
         // Loop through devices and debug log each device name and characteristics
         foreach(var device in devices)
