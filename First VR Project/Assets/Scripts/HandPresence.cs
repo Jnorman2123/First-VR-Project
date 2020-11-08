dfae14872f42c3eade6122e67552d17bbe5d2137
Jonathan Norman <Jnorman2123@gmail.com>
2020-11-08 14:24:08 -0500

    Added ability to find and instantiate a controller prefab to hand presence script
    Added ability to find and instantiate a controller prefab to hand presence script

--- First VR Project/Assets/Scripts/HandPresence.cs
+++ First VR Project/Assets/Scripts/HandPresence.cs
@@ -13,6 +13,9 @@ public class HandPresence : MonoBehaviour
     private InputDeviceCharacteristics rightControllerCharacteristics;
     // Declare targetDevice variable
     private InputDevice targetDevice;
+    // Declare new game object variables
+    private GameObject prefab;
+    private GameObject spawnedController;
     // Start is called before the first frame update
     void Start()
     {
@@ -33,6 +36,19 @@ public class HandPresence : MonoBehaviour
         if(devices.Count > 0)
         {
             targetDevice = devices[0];
+            // Set prefab to controller if controller name is equal to targetDevice name
+            prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
+            // If prefab is found set instantiate a new controller prefab and set it to spawnedController
+            if (prefab)
+            {
+                spawnedController = Instantiate(prefab, transform);
+            }
+            else
+            // If prefab is not found log error and instantiate first controller prefab and set to spawnedController
+            {
+                Debug.Log("Did not find corresponding controller model");
+                spawnedController = Instantiate(controllerPrefabs[0], transform);
+            }
         }
     }
 
