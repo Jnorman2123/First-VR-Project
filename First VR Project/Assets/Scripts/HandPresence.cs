9e68f39ba49b232e18a21da208c9f31e79202a13
Jonathan Norman <Jnorman2123@gmail.com>
2020-11-08 13:15:35 -0500

    Changed hand presence script to only get the right controller device
    Changed hand presence script to onl yget right controller device.

--- First VR Project/Assets/Scripts/HandPresence.cs
+++ First VR Project/Assets/Scripts/HandPresence.cs
@@ -7,12 +7,17 @@ public class HandPresence : MonoBehaviour
 {
     // Declare new list of devices
     private List<InputDevice> devices;
+    // Declare new set of input device characteristics
+    private InputDeviceCharacteristics rightControllerCharacteristics;
     // Start is called before the first frame update
     void Start()
     {
-        // Set devices to new list of input devices and get devices
+        // Set devices to new list of input devices and rightControllerCharacteristics
         devices = new List<InputDevice>();
-        InputDevices.GetDevices(devices);
+        rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+
+        // Get devices with right controller characteristics
+        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
 
         // Loop through devices and debug log each device name and characteristics
         foreach(var device in devices)
