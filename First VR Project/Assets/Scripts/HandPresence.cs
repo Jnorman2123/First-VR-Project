eeca319931585255e2d30d46248d49bd5077f5ff
Jonathan Norman <Jnorman2123@gmail.com>
2020-11-08 13:07:19 -0500

    Added a list of devices and a debug log of each device to hand presence script
    Added a list of devices and a debug log of each device to hand presence script

--- First VR Project/Assets/Scripts/HandPresence.cs
+++ First VR Project/Assets/Scripts/HandPresence.cs
@@ -1,13 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class HandPresence : MonoBehaviour
 {
+    // Declare new list of devices
+    private List<InputDevice> devices;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Set devices to new list of input devices and get devices
+        devices = new List<InputDevice>();
+        InputDevices.GetDevices(devices);
+
+        // Loop through devices and debug log each device name and characteristics
+        foreach(var device in devices)
+        {
+            Debug.Log(device.name + device.characteristics);
+        }
     }
 
     // Update is called once per frame
