0cc0d32c9246fe142cab0b7ccc4973fd08f791bb
Jonathan Norman <Jnorman2123@gmail.com>
2020-11-29 16:27:24 -0500

    added public XRNode variable to continous movement script
    Added a public XRNode variable to continuous movement script and set it to left hand

--- First VR Project/Assets/Scripts/ContinuousMovement.cs
+++ First VR Project/Assets/Scripts/ContinuousMovement.cs
@@ -6,6 +6,8 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class ContinuousMovement : MonoBehaviour
 {
+    // Variable for input source for movement
+    public XRNode inputSource;
     // Start is called before the first frame update
     void Start()
     {
