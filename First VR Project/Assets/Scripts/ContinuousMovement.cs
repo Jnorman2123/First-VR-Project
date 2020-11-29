eb9f1cd69770392cea946bd40ffa6b5c58b3b2a6
Jonathan Norman <Jnorman2123@gmail.com>
2020-11-29 16:38:01 -0500

    Character will now move when the left hand d pad is used
    Character will now move when the left hand d pad is used

--- First VR Project/Assets/Scripts/ContinuousMovement.cs
+++ First VR Project/Assets/Scripts/ContinuousMovement.cs
@@ -9,10 +9,14 @@ public class ContinuousMovement : MonoBehaviour
     // Variable for input source for movement and input axis vector2
     public XRNode inputSource;
     private Vector2 inputAxis;
+    // character variable and speed variable
+    private CharacterController character;
+    public float speed = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // set character variable
+        character = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -22,5 +26,11 @@ public class ContinuousMovement : MonoBehaviour
         InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
         // get value of device axis input
         device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 direction = new Vector3(inputAxis.x, 0, inputAxis.y);
+        character.Move(direction * Time.fixedDeltaTime * speed);
     }
 }
